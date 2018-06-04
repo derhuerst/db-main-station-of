98fe143e96ca438cb66f3190e4f3cd3fbee2c9df
Jannis R <mail@jannisr.de>
2018-06-04 13:24:34 +0200

    build script

--- build.js
+++ build.js
@@ -1,3 +1,28 @@
 'use strict'
 
-// todo
+const {join} = require('path')
+const readStations = require('db-stations').full
+const {writeFile} = require('fs')
+
+const dest = join(__dirname, 'index.json')
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+const byId = Object.create(null)
+
+readStations()
+.on('data', (station) => {
+	byId[station.id] = station.id
+	if (Array.isArray(station.additionalIds)) {
+		for (let id of station.additionalIds) byId[id] = station.id
+	}
+})
+.once('end', () => {
+	writeFile(dest, JSON.stringify(byId), (err) => {
+		if (err) showError(err)
+	})
+})
+.once('error', showError)
